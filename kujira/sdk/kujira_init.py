fb05bfaecce7dd4e28b2a4fd6dc3e644ea2ef73e
pauldbd <loldeadmemegod@gmail.com>
2024-11-28 18:09:46 -0600

    created sdk folder, added init

---
+++ kujira/sdk/kujira_init.py
@@ -0,0 +1,13 @@
+'''
+function to initialize the kujira set up: logs the user in and sets up a project configuration
+'''
+
+def init(
+	job_type: str | None = None,
+	project: str | None = None,
+    ):
+	print("Initializing project...")
+
+
+
+ 
